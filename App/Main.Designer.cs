93d1eec01de1899f29556bad96920f58eb22dfc7
Sangeeth Nandakumar <twileloop@outlook.com>
2023-11-25 22:35:25 +0530

    fix - Added major fixes

---
+++ App/Main.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // Browser
+            // 
+            this.Browser.ActivateBrowserOnCreation = false;
+            this.Browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Browser.Location = new System.Drawing.Point(0, 0);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(984, 585);
+            this.Browser.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 585);
+            this.Controls.Add(this.Browser);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My App";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser Browser;
+    }
+}
+
---
+++ App/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            StartServer();
+            Browser.Load("http://127.0.0.1:5000/");
+        }
+
+        private void StartServer()
+        {
+            TerminateProcessByName("DeskKiosk.Server");
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine(@"DeskKiosk.Server.exe"),
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardInput = false,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+        }
+
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            TerminateProcessByName("DeskKiosk.Server");
+        }
+
+        static void TerminateProcessByName(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            foreach (var process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                    Console.WriteLine($"Process {process.ProcessName} terminated successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error terminating process {process.ProcessName}: {ex.Message}");
+                }
+            }
+        }
+    }
+}
