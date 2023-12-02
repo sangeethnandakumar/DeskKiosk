b6228d47e08f8c3e160cd47afa0cd28e23edc9ca
Sangeeth Nandakumar <twileloop@outlook.com>
2023-12-02 10:02:57 +0530

    latch based commincation mechanism

--- deskkiosk.client/src/App.jsx
+++ deskkiosk.client/src/App.jsx
@@ -1,55 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import { HubConnectionBuilder } from "@microsoft/signalr";
-import { useEffect } from 'react';
+// App.js
+
+import { useState, useEffect } from 'react';
+import latch from './latch';
 
 function App() {
-
-    const [connection, setConnection] = useState(null);
-    useEffect(() => {
-        const connect = new HubConnectionBuilder()
-            .withUrl("http://127.0.0.1:5000/signalrhub")
-            .withAutomaticReconnect()
-            .build();
-        setConnection(connect);
-    }, []);
-
-    useEffect(() => {
-        if (connection) {
-            connection
-                .start()
-                .then(() => {
-                    connection.on("NotificationListner", (message) => {
-                        //console.log(message);
-                        setEncodedText(message);
-                    });
-                })
-                .catch((error) => console.log(error));
-        }
-    }, [connection]);
-
     const [text, setText] = useState('');
     const [encodedText, setEncodedText] = useState('');
 
-    const OnTextChange = e => {
-        setText(e.target.value);     
-        connection.invoke("SendMessage", e.target.value);
+    useEffect(() => {
+        latch.on("AppPage", "OnTextChange", msg => setEncodedText(msg));
+        latch.on("AppPage", "OnAlert", msg => alert(msg));
+    }, []);
+
+    const onTextChange = (e) => {
+        setText(e.target.value);
+        latch.invoke("AppPage", "OnTextChange", e.target.value);
     }
 
     return (
-        <div><h1>Base64 Encoder / Decoder</h1>
-
-            <textarea rows="5" cols="100" placeholder="Normal Text" value={text} onChange={OnTextChange}></textarea>
+        <div>
+            <h1>Base64 Encoder / Decoder</h1>
+            <textarea rows="5" cols="100" placeholder="Normal Text" value={text} onChange={onTextChange}></textarea>
             <br />
             <br />
             <textarea rows="5" cols="100" disabled placeholder="Encoded Text" value={encodedText}></textarea>
-            <br />
-            <br />
-            <button>DECODE</button>
-            <button>ENCODE</button>
         </div>
     );
-
 }
 
 export default App;
